a95f98fa934d8d98ffa2ea383af481e90538de6d
Mike Andrew <mikeandrew2016@gmail.com>
2022-01-11 14:54:20 +0300

    Initial Commit

---
+++ CatViewer/CatViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatViewerApp.swift
+//  CatViewer
+//
+//  Created by Meritocrat on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CatViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
